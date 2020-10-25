c0b3f012e4d90f0f3c495843e541ac013b67eb4b
Andrei Stoian <andrei.stoian35@yahoo.com>
2020-10-25 22:09:25 +0200

    New changes added

--- app/src/main/java/com/example/testapplication/LoginActivity.java
+++ app/src/main/java/com/example/testapplication/LoginActivity.java
@@ -2,28 +2,13 @@ package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Intent intent = getIntent();
-        String firstName = intent.getStringExtra("first_name");
-        String lastName = intent.getStringExtra("last_name");
-        String email = intent.getStringExtra("email");
-
-        TextView txtFirstName = findViewById(R.id.txtFirstName);
-        TextView txtLastName = findViewById(R.id.txtLastName);
-        TextView txtEmail = findViewById(R.id.txtEmail);
-
-
-        txtFirstName.setText("Your first name: " + firstName);
-        txtLastName.setText("Your first name: " + lastName);
-        txtEmail.setText("Your first name: " + email);
     }
 }
