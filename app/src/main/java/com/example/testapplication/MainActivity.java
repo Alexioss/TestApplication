660b8e718d9f361224167857f11f2fafe715e788
Andrei Stoian <andrei.stoian35@yahoo.com>
2020-10-26 23:17:26 +0200

    changes

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registerPageChange(View v){
+
+        Intent i = new Intent (this,RegisterActivity.class);
+
+        startActivity(i);
+    }
+    public void loginPageChange (View v){
+
+        Intent i = new Intent(this,LoginActivity.class);
+
+        startActivity(i);
     }
 
 
