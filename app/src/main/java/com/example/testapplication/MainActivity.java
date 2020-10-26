710d4dc85cbfb6d7549c5819815fae23b369ba05
Alexioss <alexgherghe.13@gmail.com>
2020-10-26 23:20:09 +0100

    Adding authentification

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -5,26 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginBtn = findViewById(R.id.btnLoginMain);
+    private Button regBtn = findViewById(R.id.btnRegisterMain);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    public void registerPageChange(View v){
-
-        Intent i = new Intent (this,RegisterActivity.class);
-
-        startActivity(i);
-    }
-    public void loginPageChange (View v){
-
-        Intent i = new Intent(this,LoginActivity.class);
-
-        startActivity(i);
-    }
-
 
 }
